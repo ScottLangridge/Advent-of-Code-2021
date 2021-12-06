23cf1453b38b6b63cd766af37fe6c51a47ee0cbe
Scott Langridge <scott@derossi.consulting>
2021-12-06 09:23:07 +0000

    Solved day 6

--- Day-06/day06-1.py
+++ Day-06/day06-1.py
@@ -1,16 +1,27 @@
 def main(raw_input):
-    # Parse input
+    fish = [int(x) for x in raw_input.split(',')]
 
-    # Solve problem
+    for _ in range(80):
+        fish = increment_fish(fish)
 
-    # Return solution
-    return None
+    return len(fish)
 
 
 def get_input(filename):
     with open(filename) as f:
         raw_input = f.read()
     return raw_input
+
+
+def increment_fish(fish):
+    fish_out = []
+    for i in fish:
+        if i > 0:
+            fish_out.append(i - 1)
+        else:
+            fish_out.append(6)
+            fish_out.append(8)
+    return fish_out
 
 
 if __name__ == '__main__':
