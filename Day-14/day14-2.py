063ed92529fa6d321aa66612128f8e8347a59405
Scott Langridge <scott@derossi.consulting>
2021-12-15 09:11:51 +0000

    Solved day 14 (With a hint from the subreddit for part 2)

--- Day-14/day14-2.py
+++ Day-14/day14-2.py
@@ -1,10 +1,49 @@
+from collections import defaultdict
+
+
+class PairInsertion:
+    def __init__(self, insertion_def):
+        split_def = insertion_def.split(' -> ')
+        self.left = split_def[0][0]
+        self.right = split_def[0][1]
+        self.inserted = split_def[1]
+
+    def __str__(self):
+        return f'{self.left}{self.right} -> {self.inserted}'
+
+    def matches(self, first, second):
+        return first == self.left and second == self.right
+
+
 def main(raw_input):
-    # Parse input
+    current_polymer, raw_insertions = raw_input.split('\n\n')
+    current_polymer = list(current_polymer)
+    final_char = current_polymer[-1]
+    insertions = [PairInsertion(line) for line in raw_insertions.splitlines()]
+    insertions = {(insertion.left, insertion.right): insertion for insertion in insertions}
 
-    # Solve problem
+    pair_counts = defaultdict(int)
+    for i in range(1, len(current_polymer)):
+        pair_counts[(current_polymer[i - 1], current_polymer[i])] += 1
 
-    # Return solution
-    return None
+    for _ in range(40):
+        step(pair_counts, insertions)
+
+    char_counts = defaultdict(int)
+    for pair, count in pair_counts.items():
+        char_counts[pair[0]] += pair_counts[pair]
+    char_counts[final_char] += 1
+
+    return max(char_counts.values()) - min(char_counts.values())
+
+
+def step(pair_counts, insertions):
+    frozen_pair_counts = {k: v for k, v in pair_counts.items()}
+    for pair in frozen_pair_counts.keys():
+        inserted = insertions[pair].inserted
+        pair_counts[(pair[0], inserted)] += frozen_pair_counts[pair]
+        pair_counts[(inserted, pair[1])] += frozen_pair_counts[pair]
+        pair_counts[pair] -= frozen_pair_counts[pair]
 
 
 def get_input(filename):
